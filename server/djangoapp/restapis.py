0b7aa06796cc60630c2c860c5b8d235964529c62
Priyanka Kachhap <priyanka.kmri0369@gmail.com>
2026-09-06 13:08:40 +0530

    Improve formatting and readability in restapis.py
    Refactor code for better readability and consistency in formatting.

--- server/djangoapp/restapis.py
+++ server/djangoapp/restapis.py
@@ -1,17 +1,19 @@
 import os
+
 import requests
 from dotenv import load_dotenv
+
 
 load_dotenv()
 
 backend_url = os.getenv(
-    'backend_url',
-    default="http://localhost:3030"
+    "backend_url",
+    default="http://localhost:3030",
 )
 
 sentiment_analyzer_url = os.getenv(
-    'sentiment_analyzer_url',
-    default="http://localhost:5050/"
+    "sentiment_analyzer_url",
+    default="http://localhost:5050/",
 )
 
 
@@ -23,7 +25,7 @@ def get_request(endpoint, **kwargs):
             params = params + key + "=" + value + "&"
 
     request_url = backend_url + endpoint + "?" + params
-    print("GET from {}".format(request_url))
+    print("GET from {} ".format(request_url))
 
     try:
         response = requests.get(request_url)
@@ -31,7 +33,8 @@ def get_request(endpoint, **kwargs):
     except Exception as e:
         print("Network exception occurred")
         print(e)
-        return None
+
+    return None
 
 
 def analyze_review_sentiments(text):
@@ -43,16 +46,21 @@ def analyze_review_sentiments(text):
     except Exception as e:
         print("Sentiment analysis exception occurred")
         print(e)
-        return None
+
+    return None
 
 
 def post_review(data_dict):
     request_url = backend_url + "/insert_review"
 
     try:
-        response = requests.post(request_url, json=data_dict)
+        response = requests.post(
+            request_url,
+            json=data_dict,
+        )
         return response.json()
     except Exception as e:
         print("Review posting exception occurred")
         print(e)
-        return None
+
+    return None
